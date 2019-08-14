ed82d819f3f8851e4f7867dd452843627465a4c8
Giovane Lucas <giolucas@live.com>
2019-08-14 00:23:27 -0300

    Updates

---
+++ precache-manifest.3a3a3500d519a197457394b91a59939b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd7e1c6174e7971a14c2e88c6a0bc60",
+    "url": "/GiovaneLucas/concore-test/index.html"
+  },
+  {
+    "revision": "f2ca6fde6190e83a2f13",
+    "url": "/GiovaneLucas/concore-test/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a494832de7e1312bb4ff",
+    "url": "/GiovaneLucas/concore-test/static/js/2.13c4940e.chunk.js"
+  },
+  {
+    "revision": "f2ca6fde6190e83a2f13",
+    "url": "/GiovaneLucas/concore-test/static/js/main.0f40347f.chunk.js"
+  },
+  {
+    "revision": "3561d4c32be6cebe6e46",
+    "url": "/GiovaneLucas/concore-test/static/js/runtime~main.74ffac8d.js"
+  }
+]);
